1da483258b91a28f5ace27cbc4f8ebe00145e32d
Prachi Gupta <119650267+PrachiiGuptaa@users.noreply.github.com>
2025-02-26 14:14:21 +0530

    Update About.js

--- components/About/About.js
+++ components/About/About.js
@@ -29,12 +29,13 @@ export default function About() {
       </div>
       <div className="space-y-8">
       <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4">
-          <Image src={abc} alt="Aditya Birla Capital" height={50} width={50} />
+          <Image src={Abc} alt="Aditya Birla Capital" height={50} width={50} />
           <div className="sm:w-48">
             <div className="text-[#000080] font-semibold dark:text-slate-200">Aditya Birla Sun Life Insurance, Mumbai</div>
             <div className="text-gray-600 dark:text-slate-300">Software Developer</div>
             <div className="text-gray-500 dark:text-slate-400">Nov 2024 - Present</div>
           </div>
+    </div>
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4">
           <Image src={Medisage} alt="MediSage" height={50} width={50} />
           <div className="sm:w-48">
